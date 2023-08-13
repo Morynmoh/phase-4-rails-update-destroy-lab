9814f8b1a87fa803d44c7e80cf629f96e29c091b
Maureen <morynmoh@gmail.com>
2023-08-13 14:11:25 +0300

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /plants
   def index
     plants = Plant.all
@@ -18,7 +18,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # Delete /plants
+  def destroy
+    plant = find_plant
+    plant.destroy
+    render json: { message: 'plant deleted' }, status: :no_content
+  end
+
+  # PATCH /plants
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, except: %i[updated_at created_at], status: :accepted
+  end
+
   private
+
+  def render_not_found_response
+    render json: { error: 'Not Found' }, status: :not_found
+  end
+
+  def find_plant
+    plant = Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
